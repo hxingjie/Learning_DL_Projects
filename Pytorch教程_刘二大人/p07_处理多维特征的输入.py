9e5af1226135c7cdfe3955c646d117385514b410
hxingjie <66415951+hxingjie@users.noreply.github.com>
2023-07-25 16:49:04 +0800

    Update p07_处理多维特征的输入.py

--- Pytorch教程_刘二大人/p07_处理多维特征的输入.py
+++ Pytorch教程_刘二大人/p07_处理多维特征的输入.py
@@ -1,35 +1,47 @@
 import torch
+import numpy as np
 
-x_data = torch.Tensor([[1.0], [2.0], [3.0]])
-y_data = torch.Tensor([[0], [0], [1]])
+xy = np.loadtxt('diabetes.csv.gz', delimiter=',', dtype=np.float32)
+x_data = torch.from_numpy(xy[:, :-1])
+y_data = torch.from_numpy(xy[:, [-1]])
 
 class Model(torch.nn.Module):
     def __init__(self):
-        super(LogisticRegressionModel, self).__init__()
-        self.linear = torch.nn.Linear(8, 1)# 输入是8维，输出是1维
-        self.sigmoid = torch.nn.Sigmoid()
-        
+        super(Model, self).__init__()
+        self.linear1 = torch.nn.Linear(8, 6)  # 输入是8维，输出是1维
+        self.linear2 = torch.nn.Linear(6, 4)
+        self.linear3 = torch.nn.Linear(4, 1)
+        self.activate = torch.nn.Sigmoid()
+        # self.activate = torch.nn.ReLU()
+
     def forward(self, x):
-        y_pred = self.sigmoid(self.linear(x))
-        return y_pred
+        x = self.activate(self.linear1(x))
+        x = self.activate(self.linear2(x))
+        x = self.activate(self.linear3(x))
+        return x
 
 model = Model()
 
-criterion = torch.nn.BCELoss(size_average = False)
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
+criterion = torch.nn.BCELoss(size_average=True)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 for epoch in range(1000):
+    # forward
     y_pred = model(x_data)
-    loss = criterion(y_pred,y_data)
+    loss = criterion(y_pred, y_data)
     print(epoch, loss.item())
-    
+
+    # backward
     optimizer.zero_grad()
     loss.backward()
-    optimizer.step()
-    
-print('w = ', model.linear.weight.item())
-print('b = ', model.linear.bias.item())
 
-x_test = torch.Tensor([[4.0]])
+    # update
+    optimizer.step()
+
+x_test = torch.Tensor([[-0.88, -0.15, 0.08, -0.41, 0.00, -0.21, -0.77, -0.67]])
+y_test = model(x_test)
+print('y_pred = ', y_test.data)
+
+x_test = torch.Tensor([[-0.29, 0.49, 0.18, -0.29, 0.00, 0.00, -0.53, -0.03]])
 y_test = model(x_test)
 print('y_pred = ', y_test.data)
